e595a3eb753a40ecd55c6f4d2ba676a205bc7c5f
Keith Potempa <keithrpotempa@gmail.com>
2020-01-17 12:04:28 -0600

    completed exercise

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
 
 /*
-Add logic to addExcitement that 
-[x] places an exclamation point (!) after every third word. 
+Add a new argument to the function 
+so that a developer can specify 
+which character should be displayed 
+instead of it always being an exclamation point.
 */
 
-function addExcitement (wordAry) {
+function addExcitement (wordAry, punctuation) {
     let buildMeUp = "";
     for (let i = 0; i < wordAry.length; i++) {
         if ((i + 1) % 3 === 0) {
-            buildMeUp += wordAry[i] + "! ";
+            buildMeUp += `${wordAry[i]}${punctuation} `;
         } else {
-            buildMeUp += wordAry[i] + " ";
+            buildMeUp += `${wordAry[i]} `;
         } 
         console.log(buildMeUp);
     }
 }
 
-addExcitement(sentence)
+addExcitement(sentence, "?")
